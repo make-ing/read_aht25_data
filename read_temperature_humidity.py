261a46e15197e5ed295755103be26a64887cfb83
make-ing <bckr.florian@gmail.com>
2025-12-28 16:59:28 +0100

    extract the temperature and humidity from the raw data

--- read_temperature_humidity.py
+++ read_temperature_humidity.py
@@ -1,5 +1,5 @@
 from smbus2 import SMBus, i2c_msg
-
+import struct
 
 def main():
     write_msg = i2c_msg.write(0x38, [0xac, 0x33, 0x00])
@@ -9,7 +9,21 @@ def main():
         bus.i2c_rdwr(write_msg, read_msg)
 
     data = bytes(read_msg)
-    print(data)
+    raw_humidity = (
+        (data[1] << 12) |
+        (data[2] << 4) |
+        ((data[3] & 0xF0) >> 4)
+    )
+    humidity = (raw_humidity / (1 << 20)) * 100
+    print(f"Humidity: {humidity:.2f} %")
+
+    raw_temp = (
+        ((data[3] & 0x0F) << 16) |  # lower 4 bits of byte 3
+        (data[4] << 8) |
+        data[5]
+    )
+    temperature = (raw_temp / (1 << 20)) * 200 - 50
+    print(f"Temperature: {temperature:.2f} °C")
 
 
 if __name__ == "__main__":
