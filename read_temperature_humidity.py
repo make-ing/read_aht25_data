a4a9a53770228cde40ab558188a9e22fbc62095d
make-ing <bckr.florian@gmail.com>
2025-12-28 17:04:09 +0100

    return the temperature and humidity from the function and use it to average over 10 readings before printing to stdout

--- read_temperature_humidity.py
+++ read_temperature_humidity.py
@@ -1,7 +1,8 @@
-from smbus2 import SMBus, i2c_msg
-import struct
+from time import sleep
 
-def main():
+from smbus2 import SMBus, i2c_msg
+
+def get_measurement():
     write_msg = i2c_msg.write(0x38, [0xac, 0x33, 0x00])
     read_msg = i2c_msg.read(0x38, 7)
 
@@ -9,13 +10,13 @@ def main():
         bus.i2c_rdwr(write_msg, read_msg)
 
     data = bytes(read_msg)
+
     raw_humidity = (
         (data[1] << 12) |
         (data[2] << 4) |
         ((data[3] & 0xF0) >> 4)
     )
     humidity = (raw_humidity / (1 << 20)) * 100
-    print(f"Humidity: {humidity:.2f} %")
 
     raw_temp = (
         ((data[3] & 0x0F) << 16) |  # lower 4 bits of byte 3
@@ -23,8 +24,15 @@ def main():
         data[5]
     )
     temperature = (raw_temp / (1 << 20)) * 200 - 50
-    print(f"Temperature: {temperature:.2f} °C")
+    return humidity, temperature
 
 
 if __name__ == "__main__":
-    main()
+    humidity_sum, temperature_sum = 0, 0
+    for _ in range(10):
+        humidity, temperature = get_measurement()
+        humidity_sum += humidity
+        temperature_sum += temperature
+        sleep(0.1)
+    print(f"Humidity: {humidity_sum / 10:.1f} %")
+    print(f"Temperature: {temperature_sum / 10:.1f} °C")
